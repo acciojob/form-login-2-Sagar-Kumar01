929d919bce1322094f33280a4510a20a7dff1108
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-13 12:28:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let forms = document.getElementById("forms");
+
+forms.addEventListener("submit",()=>{
+	let fname = document.getElementById("FirstName");
+	let lname = document.getElementById("LastName");
+	let ph = document.getElementById("PhoneNumber");
+	let eid = document.getElementById("EmailID");
+	alert(`${fname.name} ${fname.value} ${lname.name} ${lname.value} ${ph.name} ${ph.value} ${eid.name} ${eid.value}`)
+})
